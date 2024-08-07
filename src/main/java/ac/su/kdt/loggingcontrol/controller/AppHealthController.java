ac0b5a4aa1213d80a2ac7b81ec4af3e988970d24
gayeonni <gayeoneez@gmail.com>
2024-08-07 13:57:45 +0900

    Update AppHealthController.java

--- src/main/java/ac/su/kdt/loggingcontrol/controller/AppHealthController.java
+++ src/main/java/ac/su/kdt/loggingcontrol/controller/AppHealthController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppHealthController {
     @GetMapping("/app-health")
     public String healthCheck() {
-        return "app_health_status 1.0\n# EOF ";
+        return "app_health_status 1.0\n# EOF";
     }
 }
